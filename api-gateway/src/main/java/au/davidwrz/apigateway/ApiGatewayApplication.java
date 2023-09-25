8bc07854c0c1ab8110270df8c197dba629160f22
Dawid Wrzos <wrzosdawid@wp.pl>
2023-09-25 21:51:03 +0200

    Create api gateway microservice

---
+++ api-gateway/src/main/java/au/davidwrz/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package au.davidwrz.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
