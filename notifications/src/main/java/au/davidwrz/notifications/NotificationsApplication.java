c92488538bae40102ec6970e890bbc97b4c0621b
Dawid Wrzos <wrzosdawid@wp.pl>
2023-10-08 13:15:10 +0200

    Implement profiles for each microservice

--- notifications/src/main/java/au/davidwrz/notifications/NotificationsApplication.java
+++ notifications/src/main/java/au/davidwrz/notifications/NotificationsApplication.java
@@ -2,6 +2,8 @@ package au.davidwrz.notifications;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {
@@ -9,6 +11,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
                 "au.davidwrz.amqp",
         }
 )
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 class NotificationsApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationsApplication.class, args);
